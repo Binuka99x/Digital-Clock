7b90716ad6dcc27eaf239064a872e51bbe8147ae
Binuka99x <binukag1@.com>
2023-12-29 09:36:00 +0530

    add 0 to if number less than 10

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 setInterval(updateClock, 1000);
-function updateClock(){
+function updateClock() {
     let myDate = new Date();
     let myHours = myDate.getHours();
     let myMinutes = myDate.getMinutes();
     let mySeconds = myDate.getSeconds();
+
+    if (myHours < 10) { myHours = "0" + myHours };
+    if (myMinutes < 10) { myMinutes = "0" + myMinutes };
+    if (mySeconds < 10) { mySeconds = "0" + mySeconds };
+
     let myClock = myHours + ":" + myMinutes + ":" + mySeconds;
     document.getElementById("clock").innerText = myClock;
 }
